dbde1316476ab5a2c8806515679b34fe26cfc8c6
Peter Rifkind <prifkind@gmail.com>
2021-12-29 23:06:20 -0500

    Added state to functional component Accordion

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  return (
-    <div>
-      <h1>{items.length}</h1>
-    </div>
-  );
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className={`title ${active}`}
+          onClick={() => {
+            onTitleClick(index);
+          }}
+        >
+          <icon className="dropdown icon"></icon>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
