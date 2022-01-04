b945a859b17d33e19deb79cf5ce28bc97ff629ef
Peter Rifkind <prifkind@gmail.com>
2022-01-03 23:06:37 -0500

    Added Wikipedia API call; rendering of API results"

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-        console.log('useEffect')
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderResults = results.map((e) => {
+    return (
+      <div className="item" key={e.pageid}>
+        <div className="content">
+          <div className="header">{e.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: e.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -19,9 +47,9 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderResults}</div>
     </div>
   );
 };
 
 export default Search;
-//
