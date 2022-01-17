248a99385469e9dd0e3ef9974b32614f34da8d35
Peter Rifkind <prifkind@gmail.com>
2022-01-16 23:28:51 -0500

    Added and began factoring navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
-// import Accordion from "./Components/Accordion";
-// import Search from "./Components/Search";
+import React, { useState } from "react";
+import Accordion from "./Components/Accordion";
+import Search from "./Components/Search";
 import Dropdown from "./Components/Dropdown";
-import { useState } from "react";
 import Translate from "./Components/Translate";
+import Route from "./Components/Route";
+import Header from "./Components/Header";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 const options = [
   {
@@ -21,9 +37,27 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path={"/"}>
+        <Accordion items={items} />
+      </Route>
+      <Route path={"/list"}>
+        <Dropdown
+          label="Select a name"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path={"/search"}>
+        <Search />
+      </Route>
+      <Route path={"/translate"}>
+        <Translate />
+      </Route>
     </div>
   );
 };
